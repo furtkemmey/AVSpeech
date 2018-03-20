7568c239b312088652fa6aacc39e93e4f2f9cf3c
furtkemmey <furtkemmey@yahoo.com.tw>
2018-03-20 20:14:39 +0800

    worked

--- AVSpeech/ViewController.swift
+++ AVSpeech/ViewController.swift
@@ -12,37 +12,50 @@ import MediaPlayer
 //import AVKit
 
 class ViewController: UIViewController {
-    let synth = AVSpeechSynthesizer()
+    var synth: AVSpeechSynthesizer?
 //    var  utterance:AVSpeechUtterance?
-    let audioSession = AVAudioSession.sharedInstance()
+//    let audioSession = AVAudioSession.sharedInstance()
 
     @IBAction func button(_ sender: UIButton) {
-        let utterance = AVSpeechUtterance(string: "Unity")
-//        utterance = AVSpeechUtterance(string: "Unity")
-        utterance.rate = 0.1 // AVSpeechUtteranceDefaultSpeechRate
-        utterance.pitchMultiplier = 1.0 // 0.5 ~ 2.0
-        utterance.preUtteranceDelay = 0.1
-//        utterance?.postUtteranceDelay = 0.1
-        utterance.volume = 0.1 // default 1 0.0 ~ 1.0
-        utterance.voice = AVSpeechSynthesisVoice(language: "en-US") //zh-TW
-        synth.speak(utterance)
-        synth.delegate = self
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let audioSession = AVAudioSession.sharedInstance()
         do {
             // 設置語 環境，保證能朗讀出聲 (特別是剛做過語 識別，這 話必加，不然沒聲 )
-            //        try audioSession.setCategory(AVAudioSessionCategoryAmbient)
-            try audioSession.setCategory(AVAudioSessionCategoryPlayback) // AVAudioSessionCategoryPlayAndRecord
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback) // AVAudioSessionCategoryPlayAndRecord AVAudioSessionCategoryPlayback AVAudioSessionCategoryAmbient AVAudioSessionCategorySoloAmbient
             try AVAudioSession.sharedInstance().setActive(true)
-//            try AVAudioSession.sharedInstance().setActive(false)
-//            try audioSession.setMode(AVAudioSessionModeDefault)
+            try audioSession.setMode(AVAudioSessionModeSpokenAudio) // AVAudioSessionModeSpokenAudio AVAudioSessionModeDefault AVAudioSessionCategoryPlayback
 
         }catch let error as NSError {
             print(error.code)
-
         }
+
+        let utterance = AVSpeechUtterance(string: "生氣了aaa")
+//        utterance = AVSpeechUtterance(string: "Unity")
+        utterance.rate = AVSpeechUtteranceDefaultSpeechRate // AVSpeechUtteranceDefaultSpeechRate 0.1
+        utterance.pitchMultiplier = 1.0 // 0.5 ~ 2.0
+        utterance.preUtteranceDelay = 0.3
+        utterance.postUtteranceDelay = 0.3
+        utterance.volume = 1 // default 1 0.0 ~ 1.0
+        utterance.voice = AVSpeechSynthesisVoice(language: "zh-TW") //zh-TW en-US
+            self.synth?.speak(utterance)
+//        synth?.delegate = self
+
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        synth = AVSpeechSynthesizer()
+        synth = AVSpeechSynthesizer()
+//        synth?.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
+//        do {
+//            // 設置語 環境，保證能朗讀出聲 (特別是剛做過語 識別，這 話必加，不然沒聲 )
+//            //        try audioSession.setCategory(AVAudioSessionCategoryAmbient)
+//            try audioSession.setCategory(AVAudioSessionCategoryPlayback) // AVAudioSessionCategoryPlayAndRecord AVAudioSessionCategoryPlayback
+//            try AVAudioSession.sharedInstance().setActive(true)
+////            try audioSession.setMode(AVAudioSessionModeDefault)
+//
+//        }catch let error as NSError {
+//            print(error.code)
+//        }
     }
 
 }
